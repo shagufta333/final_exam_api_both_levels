9eb465c4f8440138bd2835b4835192c34872ee13
shagufta <shagufta.batool8@gmail.com>
2022-05-19 11:53:07 +0200

    create test spec for comment model

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :comment do
     body { "MyString" }
+    article
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,19 @@
-require 'rails_helper'
+RSpec.describe Rating, type: :model do
+  describe 'Products database Table check' do
+    it { is_expected.to have_db_column(:body).of_type(:string) }
+  end
 
-RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :body }
+  end
+
+  describe 'Associations' do
+    it { is_expected.to belong_to :article }
+  end
+
+  describe 'Factory' do
+    it 'is expected to be valid' do
+      expect(create(:comment)).to be_valid
+    end
+  end
 end
